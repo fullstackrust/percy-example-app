ff576e3470e936195c70d304b902a959b0363b13
Hunter Trujillo <cryptoquick@pm.me>
2019-03-01 15:22:16 -0700

    Add more inputs to the form.

--- app/src/views/management_view.rs
+++ app/src/views/management_view.rs
@@ -3,7 +3,7 @@ use crate::state::Msg;
 use crate::store::Store;
 use crate::views::nav_bar_view::NavBarView;
 use css_rs_macro::css;
-use std::cell::RefCell;
+use std::cell::{Ref, RefCell};
 use std::rc::Rc;
 use virtual_dom_rs::prelude::*;
 use wasm_bindgen::JsCast;
@@ -15,6 +15,31 @@ static WRAP_CSS: &'static str = css! {"
         padding: 20px 30px;
     }
 "};
+
+fn field_name(name: &str) -> Rc<RefCell<String>> {
+    Rc::new(RefCell::new(name.to_string()))
+}
+
+fn form_input(store: Rc<RefCell<Store>>, name: Rc<RefCell<String>>) -> VirtualNode {
+    html! {
+        <input oninput=move |event: Event| {
+            let input_elem = event.target().unwrap();
+            let input_elem = input_elem.dyn_into::<HtmlInputElement>().unwrap();
+            let value: String = input_elem.value();
+            let store: Rc<RefCell<Store>> = Rc::clone(&store);
+            let name: Rc<RefCell<String>> = Rc::clone(&name);
+            store.borrow_mut().msg(&Msg::Input(name.borrow().to_string(), value));
+        } />
+    }
+}
+
+pub fn get_field(store: Ref<Store>, name: &str) -> String {
+    store
+        .form()
+        .get("job_name")
+        .unwrap_or(&"".to_string())
+        .to_owned()
+}
 
 // Page
 pub struct ManagementView {
@@ -30,37 +55,30 @@ impl ManagementView {
 impl View for ManagementView {
     fn render(&self) -> VirtualNode {
         let nav_bar = NavBarView::new(ActivePage::Management, Rc::clone(&self.store)).render();
-        let job_name_field: String = self
-            .store
-            .borrow()
-            .form()
-            .get("job_name")
-            .unwrap_or(&"".to_string())
-            .to_owned();
-
-        let store: Rc<RefCell<Store>> = Rc::clone(&self.store);
 
         html! {
             <div>
                 { nav_bar }
                 <div class=WRAP_CSS>
                     <h2>Management</h2>
-                    <div>
-                        <input
-                            oninput=move |event: Event| {
-                                let input_elem = event.target().unwrap();
-                                let input_elem = input_elem.dyn_into::<HtmlInputElement>().unwrap();
-                                let job_name_value = input_elem.value();
-                                let store: Rc<RefCell<Store>> = Rc::clone(&store);
-                                store
-                                    .borrow_mut()
-                                    .msg(&Msg::Input("job_name".to_string(), job_name_value.to_string()));
-                            }
-                        />
-                        <div>
-                            { text!(job_name_field) }
-                        </div>
-                    </div>
+                    <table>
+                        <tr>
+                            <td><label>Job name</label></td>
+                            <td>{ form_input(Rc::clone(&self.store), field_name("job_name")) }</td>
+                        </tr>
+                        <tr>
+                            <td><label>Job description</label></td>
+                            <td>{ form_input(Rc::clone(&self.store), field_name("job_desc")) }</td>
+                        </tr>
+                        <tr>
+                            <td><label>Job assigned to</label></td>
+                            <td>{ form_input(Rc::clone(&self.store), field_name("job_user")) }</td>
+                        </tr>
+                        <tr>
+                            <td><label>Job rate</label></td>
+                            <td>{ form_input(Rc::clone(&self.store), field_name("job_rate")) }</td>
+                        </tr>
+                    </table>
                 </div>
             </div>
         }
